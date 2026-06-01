2fc73bba74079bceed113aab2719b7832e99ef7f
sanskruti <sanskrutidiggikar39@gmail.com>
2026-06-01 22:51:16 +0530

    Add print statement for displaying results

--- operations.py
+++ operations.py
@@ -4,6 +4,7 @@ addition= num1+num2
 subtraction= num1-num2  
 multiplication= num1*num2
 division= num1/num2
+print("THE ANSWER ARE:")
 print("Addition: ", addition)
 print("Subtraction: ", subtraction)
 print("Multiplication: ", multiplication)
