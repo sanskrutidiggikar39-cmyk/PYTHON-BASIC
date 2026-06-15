c70e70c9b35c1f991dc3d53187de3662f309cd8c
sanskruti <sanskrutidiggikar39@gmail.com>
2026-06-15 23:17:36 +0530

    Add modulus operation to calculations

--- operations.py
+++ operations.py
@@ -4,8 +4,10 @@ addition= num1+num2
 subtraction= num1-num2  
 multiplication= num1*num2
 division= num1/num2
+modulus= num1%num2
 print("THE ANSWER ARE:")
 print("Addition: ", addition)
 print("Subtraction: ", subtraction)
 print("Multiplication: ", multiplication)
 print("Division: ", division)      
+print("Modulus: ", modulus)
