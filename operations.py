d3fe916beb7ccf055ee1e5536d9ba77e87693d77
sanskruti <sanskrutidiggikar39@gmail.com>
2026-06-25 23:54:03 +0530

    Fix syntax error in modulus print statement

--- operations.py
+++ operations.py
@@ -10,4 +10,4 @@ print("Addition: ", addition)
 print("Subtraction: ", subtraction)
 print("Multiplication: ", multiplication)
 print("Division: ", division)      
-print("Modulus: ", modulus
+print("Modulus: ", modulus)
